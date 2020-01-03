68fea7816fcbbbcd075f08b3294596e148763680
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 18:13:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter <= array.size do
-    new_array << (array.shift ** 2)
+    new_array << (array. ** 2)
     counter +=1
   end
   p new_array
